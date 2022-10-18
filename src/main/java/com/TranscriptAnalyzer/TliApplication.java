9951b588ae91dc2cde7de28fc8fd65b152fe3090
rumaisachowdhury <rumaisa.chowdhury@mail.utoronto.ca>
2022-10-17 23:57:32 -0400

    New Starter Pack

---
+++ src/main/java/com/TranscriptAnalyzer/TliApplication.java
@@ -0,0 +1,13 @@
+package com.TranscriptAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TranscriptAnalyzer/tli/TliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TranscriptAnalyzer.tli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
